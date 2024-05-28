41f2924b39c614acc9d660023e285dcdec96a70c
Rajib Mandal <rajibmandal@matrixnmedia.com>
2024-05-28 12:05:11 +0530

    middleware update.

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$|login).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$|login|register).*)"],
 };
